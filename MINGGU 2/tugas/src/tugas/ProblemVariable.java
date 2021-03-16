2c8d1e7b2782b135e8b8ddd413af80e13fb51c47
Siska1407 <ladyrose1407@gmail.com>
2021-03-16 15:28:55 +0700

    tugas

---
+++ MINGGU 2/tugas/src/tugas/ProblemVariable.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas;
+
+public class ProblemVariable {
+    public static void main(String[] args) {
+        //deklarasi variable yang benar
+        
+        boolean gameOver = false;
+        int students = 50, classes = 3;
+        double sales_tax;
+        short number1;
+        
+        //deklarasi variabel yang salah
+        int 2berOrNot2be;
+        float price index;
+        double lastYear'sPrice;
+        long class;
+        
+        
+    }    
+}
---
+++ MINGGU 2/tugas/src/tugas/person1.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas;
+
+/**
+ *
+ * @author user
+ */
+public class person1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Initialize variable
+        String fName, lName, stuId, stuStatus;
+        fName       = "Siska";
+        lName       = "Yulia Rahma"; 
+        stuId       = "E41200999";
+        stuStatus   = "Active";
+        
+        //show
+        System.out.println("student Name\t: " + fName + "" + lName);
+        System.out.println("student ID\t: " + stuId);
+        System.out.println("student status\t: " + stuStatus);
+    }
+    
+}
